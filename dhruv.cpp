1fb8b802a500359c18f5b05b0a7cd6199e7b5fe4
Dhruv Arora <78024428+Dhruv-Arora-Git@users.noreply.github.com>
2021-10-09 18:34:53 +0530

    my template for CP

---
+++ dhruv.cpp
@@ -0,0 +1,35 @@
+ /*
+ ___
+|   \  | |__    _,_           __     __   
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+// ----------------------------------------------
+
+
+void dhruv(){
+	
+}
+
+int main(){
+	fast;
+	//cout<<fixed<<setprecision(9);
+	ll t; cin >> t;
+	while(t--){
+		// solve function
+		dhruv();
+	}
+	return 0;
+}
